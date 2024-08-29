b5be9201384781ed84e340a5fda8dc2582f08706
Peter Kloke <peter.kloke@hhi.fraunhofer.de>
2024-08-29 17:43:30 +0200

    Deploy website - based on 403f6d7c883bc45176ec6d05f0eb9a3f736b2e9a

--- assets/js/acecf23e.8d75015e.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[1903],{1912:s=>{s.exports=JSON.parse('{"blogBasePath":"/NaLamKI-Docs/blog","blogTitle":"Blog","authorsListPath":"/NaLamKI-Docs/blog/authors"}')}}]);
--- assets/js/c813f75a.6bf1eb38.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[2333],{429:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/NaLamKI-Docs/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
